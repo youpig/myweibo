d785b39ec2e2777dd406702176f0e7b886bdcf5c
wangqingyan <wangqingyan222@gmail.com>
2016-01-24 22:29:21 +0800

    解决tabbar 渲染 文字为 蓝色的问题

--- weibo/weibo/QYTabbarViewController.m
+++ weibo/weibo/QYTabbarViewController.m
@@ -17,38 +17,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    
+    
+    
     UIViewController *home = [[UIViewController alloc] init];
-    [self addChildViewController:home];
-    home.tabBarItem.title = @"首页";
-    home.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
-    home.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_home_selected"];
-    home.view.backgroundColor = [UIColor grayColor];
+    [self addChildViewController:home withTitle:@"首页" image:@"tabbar_home"];
     
     UIViewController *message = [[UIViewController alloc] init];
-    [self addChildViewController:message];
-    message.tabBarItem.title = @"消息";
-    message.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
-    message.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_message_center_selected"];
-    message.view.backgroundColor = [UIColor blueColor];
+    [self addChildViewController:message withTitle:@"消息" image:@"tabbar_message_center"];
     
     UIViewController *discover = [[UIViewController alloc] init];
-    [self addChildViewController:discover];
-    discover.tabBarItem.title = @"发现";
-    discover.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
-    discover.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_discover_selected"];
-
-    discover.view.backgroundColor = [UIColor redColor];
+    [self addChildViewController:discover withTitle:@"发现" image:@"tabbar_discover"];
     
     UIViewController *me = [[UIViewController alloc] init];
-    [self addChildViewController:me];
-    me.tabBarItem.title = @"我";
-    me.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
-    me.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_profile_selected"];
-
-    me.view.backgroundColor = [UIColor purpleColor];
+    [self addChildViewController:me withTitle:@"我" image:@"tabbar_profile"];
     
 }
 
 
+- (void)addChildViewController:(UIViewController *)controller withTitle:(NSString *)title image:(NSString *)image{
+
+    controller.tabBarItem.title = title;
+    controller.tabBarItem.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@",image]];
+    
+#warning 手动关闭对图片的渲染,默认是渲染为蓝色
+    UIImage *seletedImg = [[UIImage imageNamed:[NSString stringWithFormat:@"%@_selected",image]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    controller.tabBarItem.selectedImage = seletedImg;
+    
+#warning 解决文字被渲染的问题 NSForeGroundColorAttributeName
+    NSDictionary *attrs = @{NSForegroundColorAttributeName:[UIColor orangeColor]};
+    [controller.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateSelected];
+    
+    [self addChildViewController:controller];
+
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
