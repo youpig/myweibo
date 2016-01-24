e4910ea038e3ec7d34dda23bded0ad4eeaebfbcc
wangqingyan <wangqingyan222@gmail.com>
2016-01-24 20:58:10 +0800

    增加了tabbarItem的selected 状态的图片

--- weibo/weibo/QYTabbarViewController.m
+++ weibo/weibo/QYTabbarViewController.m
@@ -21,24 +21,30 @@
     [self addChildViewController:home];
     home.tabBarItem.title = @"首页";
     home.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
+    home.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_home_selected"];
     home.view.backgroundColor = [UIColor grayColor];
     
     UIViewController *message = [[UIViewController alloc] init];
     [self addChildViewController:message];
     message.tabBarItem.title = @"消息";
     message.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
+    message.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_message_center_selected"];
     message.view.backgroundColor = [UIColor blueColor];
     
     UIViewController *discover = [[UIViewController alloc] init];
     [self addChildViewController:discover];
     discover.tabBarItem.title = @"发现";
     discover.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
+    discover.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_discover_selected"];
+
     discover.view.backgroundColor = [UIColor redColor];
     
     UIViewController *me = [[UIViewController alloc] init];
     [self addChildViewController:me];
     me.tabBarItem.title = @"我";
     me.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
+    me.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_profile_selected"];
+
     me.view.backgroundColor = [UIColor purpleColor];
     
 }
